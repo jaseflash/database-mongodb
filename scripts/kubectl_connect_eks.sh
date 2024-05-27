c3c46a0491ac15e1efb4241695edb9fd5b877964
jaseflash <84444515+jaseflash@users.noreply.github.com>
2024-05-27 19:30:49 +0100

    k8s services

---
+++ scripts/kubectl_connect_eks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+# Setup local AWS Env variables
+if [[ -z $1 ]]; then  #Pass path for tfstate dir if not in quickstart.
+output=$(terraform output -state $SCRIPT_DIR/../quickstart/infra/terraform.tfstate -json)
+else
+output=$(terraform output -state ${1}/terraform.tfstate -json)
+fi
+
+PROFILE="assumed-role"
+PROJECTS=($(echo $output | jq -r '. | to_entries[] | select(.key|endswith("_projects")) | .value.value[]'))
+EKS_CLUSTER_NAMES=$(echo $output | jq -r ".| to_entries[] | select(.key|endswith(\"_eks_cluster_names\")) | .value.value | to_entries[] | (.value)")
+REGIONS=$(echo $output |jq -r ".| to_entries[] | select(.key|endswith(\"_regions\")) | .value.value | to_entries[] | (.value)")
+
+echo "Authenticating to EKS Cluster ${EKS_CLUSTER_NAMES} - ${REGIONS}"
+# get identity
+aws sts get-caller-identity
+# add EKS cluster to $HOME/.kube/config
+echo "aws eks --region $REGIONS update-kubeconfig --name $EKS_CLUSTER_NAMES --alias \"${EKS_CLUSTER_NAMES##*-}\" "
+# aws eks --region $REGIONS update-kubeconfig --name $EKS_CLUSTER_NAMES --alias "${EKS_CLUSTER_NAMES##*-}" 
+echo
+echo "Create Aliases"
+echo "  alias: ${EKS_CLUSTER_NAME} =  kubectl config use-context ${CLUSTER_NAME} # ${EKS_CLUSTER_NAMES##*-}"
+# alias $(echo ${EKS_CLUSTER_NAME})="kubectl config use-context ${CLUSTER_NAME} # ${EKS_CLUSTER_NAMES##*-}"
+alias 'kt=kubectl -n tasky'
+alias 'kk=kubectl -n kube-system'
+alias 'k=kubectl'
