f348c06a5cb4f93c879c71764f2c41a81b8c4f0b
jaseflash <84444515+jaseflash@users.noreply.github.com>
2024-06-05 19:09:30 +0100

    trivy

--- aws-tasky/scripts/scan.sh
+++ aws-tasky/scripts/scan.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-# K8s Namespace Permissions Audit
-echo "Running NamespaceHound on tasky namespace"
-python3 /Users/jasonbroadbent/Projects/security/namespacehound/nshound.py --kubeconfig /Users/jasonbroadbent/.kube/config -o table
-#python3 /Users/jasonbroadbent/Projects/security/namespacehound/nshound.py --kubeconfig /Users/jasonbroadbent/.kube/config -n tasky -o table
-#python3 /Users/jasonbroadbent/Projects/security/namespacehound/nshound.py --kubeconfig /Users/jasonbroadbent/.kube/config -c -o table
-echo
+rivy container and namespace testing
 echo "Running trivy image scan on tasky"
 trivy image --severity HIGH,CRITICAL jaseflash1234/tasky:1.3
 echo
 echo "Running trivy k8s deployment scan on tasky namespace"
-trivy k8s --namespace=tasky --report=summary deploy
+trivy k8s --include-namespaces kube-system --report summary 
+trivy k8s --include-namespaces tasky --report summary
 echo
 echo "Running trivy aws scan to see S3 bucket findings"
 trivy aws
-echo "running trivy repo scan on aws-tasky"
-trivy repo ${SCRIPT_DIR}/../../aws-tasky
