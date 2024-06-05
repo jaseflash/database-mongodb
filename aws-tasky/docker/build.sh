b59ac51d34e4a18127a8dd6031c6f36cc6bc1069
jaseflash <84444515+jaseflash@users.noreply.github.com>
2024-06-05 16:25:25 +0100

    webhook issue

--- aws-tasky/docker/build.sh
+++ aws-tasky/docker/build.sh
@@ -4,5 +4,5 @@ mkdir ${SCRIPT_DIR}/tasky
 git clone https://github.com/jeffthorne/tasky ./tasky
 cp ${SCRIPT_DIR}/Dockerfile.chainguard-static ${SCRIPT_DIR}/tasky/
 cp ${SCRIPT_DIR}/wizexercise.txt ${SCRIPT_DIR}/tasky/
-docker build -t ppresto/tasky:1.3 -f ${SCRIPT_DIR}/tasky/Dockerfile.chainguard-static ${SCRIPT_DIR}/tasky/
-#docker push ppresto/tasky:1.3
+docker build -t jaseflash1234/tasky:1.3 -f ${SCRIPT_DIR}/tasky/Dockerfile.chainguard-static ${SCRIPT_DIR}/tasky/
+#docker push jaseflash1234/tasky:1.3
